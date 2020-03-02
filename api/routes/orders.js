e2094b264bbcba1a50f3acac7a489cf5b8ffefaa
Andrii <andrii.chemer@artatech.pl>
2020-03-02 11:58:19 +0100

    Added crud operation with Mongo DB (Database is remote)

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,8 +9,13 @@ router.get('/', (req, res, nexr) => {
 });
 
 router.post('/', (req, res, nexr) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
     res.status(201).json({
-        message: 'Orders was created'
+        message: 'Orders was created',
+        order: order
     });
 });
 
--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const produstRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders')
 
+mongoose.connect(
+    'mongodb+srv://node-shop:' + 
+    process.env.MONGO_ATLAS_PW + 
+    '@node-rest-shop-ikat2.mongodb.net/test?retryWrites=true&w=majority', 
+    { useNewUrlParser: true }
+);
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 //Routes which should handle requests
 app.use('/products', produstRoutes);
