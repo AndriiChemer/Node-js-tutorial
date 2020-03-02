8e010e0c69b00b3eb9cbbb578a62bf8f106da992
Andrii <andrii.chemer@artatech.pl>
2020-03-02 15:30:56 +0100

    uploading image to server

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,20 +1,46 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './uploads/');
+    }, 
+    filename(req, file, callback) {
+        callback(null, new Date().toISOString() + file.originalname);
+    }
+});
+const fileFilter = (req, file, callback) => {
+    // reject a file
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        callback(null, true);
+    } else {
+        callback(null, false);
+    }
+}
 
+const upload = multer({
+    storage: storage, 
+    limits: { 
+        fileSize: 1024 * 1024 * 5 // 5 = megabyte
+    },
+    fileFilter: fileFilter
+});
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     Product.find()
-        .select('name price _id')
+        .select('name price _id productImage')
         .exec()
         .then(docs => {
             const response = {
                 count: docs.length,
                 products: docs.map(doc => {
+                    var imageUrl = req.get('host') + '/' + doc.productImage
                     return {
                         name: doc.name,
                         price: doc.price,
+                        productImage: imageUrl,
                         _id: doc._id,
                         request: {
                             type: 'GET',
@@ -33,11 +59,12 @@ router.get('/', (req, res, next) => {
         });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('productImage'), (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
+        price: req.body.price,
+        productImage: req.file.path
     });
 
     // save() - save to database
