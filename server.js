66a4b0b2621c8d19047e528a49b6e36e487e4156
Andrii <andrii.chemer@artatech.pl>
2020-03-11 16:59:08 +0100

    test 2

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const http = require('http');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-const server = http.createServer(app);
+app.listen(app);
+// const server = http.createServer(app);
 
-server.listen(port, 'https://freebookstest.herokuapp.com/', () => {console.log('App is listening on port 3000!')});
+// server.listen(port);
 // server.listen(port, '192.168.80.222' || 'localhost', () => {console.log('App is listening on port 3000!')});
